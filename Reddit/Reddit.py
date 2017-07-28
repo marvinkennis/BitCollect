c590a7bfc6d47d9b7e32bcb5e91d01282f37295e
Marvin Kennis <marvinkennis@gmail.com>
2017-07-28 17:15:04 +0200

    Add comments + Remove unused var assignments
    Added some comments to the Reddit scraper, removed unused variable
    assignments

--- Reddit/Reddit.py
+++ Reddit/Reddit.py
@@ -25,10 +25,14 @@ sys.setdefaultencoding("utf8")
 
 #Change this to CSV writer
 def saveSubmission(submission, filename):
+    #Reddit accounts can be deleted, and then the username won't show. If it doesn't exist fill in [deleted]
     if not submission.author:
         name = '[deleted]'
+    #Otherwise just assign the author name 
     else:
         name = submission.author.name
+
+    #Write it to a CSV 
     csvwriter = csv.writer(open(filename, "a"))
     csvwriter.writerow([datetime.datetime.fromtimestamp(submission.created).strftime('%c'), submission.permalink,  name, submission.title, submission.selftext, submission.score])
 
@@ -36,29 +40,45 @@ def saveSubmission(submission, filename):
 # ts_interval - the interval in seconds for each request cycle
 # largest_timestamp - if not set, is set to 12 hours from now.
 def getThreads(subredditName, ts_interval, ep1, ep2, user_ag, user):
+    #Set filename for this subreddit. Timestamp it so everytime this script is run, results are appended to differnt file 
     filename = 'Reddit_'+subredditName+'_'+str(int(time.time()))+'.csv'
+
+    #Creating header rows in the CSV file 
+    csvwriter = csv.writer(open(filename, "a"))
+    csvwriter.writerow(['timestamp', 'url',  'author', 'postTitle', 'postContent', 'score'])
+    #Initialize the Reddit client with client ID, secret key, user agent (arbitrary), password and username
+    #See the included readme for detailed explanation 
     reddit = praw.Reddit(client_id=user['clientID'],client_secret=user['secretKey'],user_agent=user_ag,password=user['password'],username=user['username'])
     print(reddit.user.me())
     #If no time epoch is specified, 
     if ep2 is None:
         ep2 = int(time.time()) + 12*3600
+
+    #cts for current timestamp. Defines the sliding window between which the search query is executed. 
     cts2 = ep2
+    #Substract the ts interval from the second timstamp to move the window
     cts1 = ep2 - ts_interval
     currentTSInterval = ts_interval
     while True:
+        #Use reddit search API to search for posts between two timestamps (created above), on the subreddit defined in subRedditName
+        #Using cloudsearch as syntax, because otherwise it is not possible to search between timestamps 
         try:
             searchResults = list(reddit.subreddit(subredditName).search('timestamp:{}..{}'.format(cts1, cts2), syntax='cloudsearch'))
+        #If it errors for whatever reason, log the error and continue 
         except Exception as e:
-            #logging.exception(e)
+            logging.exception(e)
             continue
 
         #logging.debug("Got {} submissions in interval {}..{}".format(len(searchResults), cts1, cts2))
+        #It returns at most 100 search results, so if it maxes out, we split the interval and search again 
+        #This way we ensure that we do not miss any posts 
         if len(searchResults) == 100:
             currentTSInterval /= 2
             cts1 = cts2 - currentTSInterval
            # logging.debug("Reducing time interval to {}".format(currentTSInterval))
             continue
 
+        #Printing Reddit post title to show progress 
         for submission in searchResults:
             print submission.title
             saveSubmission(submission, filename)
@@ -76,8 +96,6 @@ def getThreads(subredditName, ts_interval, ep1, ep2, user_ag, user):
            # logging.debug("Increasing time interval to {}".format(currentTSInterval))
 
 def main():
-    epc = int(time.time()) + 12*3600
-    epb = int(time.time()) - 12*3600
     parser = argparse.ArgumentParser(description='Parallel Subreddit Downloader')
     parser.add_argument("--subreddit", dest="subreddit", required=True, help="Download the whole subreddit")
     parser.add_argument("--timestamp_interval", dest="timestamp_interval", type=int, required=True)
@@ -103,6 +121,7 @@ def main():
     ep1 = [0]*8
     ep2 = [0]*8
     processes = []
+
     for i in range(0, 8):
         ep1[i] = epoch1 + i*inc
         ep2[i] = epoch1 + (i + 1)*inc - 1
--- Reddit/settings.py
+++ Reddit/settings.py
@@ -1,4 +1,4 @@
-config = {'username':'REPLACE',
-        'password': 'REPLACE',
-        'clientID': 'REPLACE',
-        'secretKey':'REPLACE'}
+config = {'username':'YOUR REDDIT USERNAME HERE',
+        'password': 'YOUR REDDIT PASSWORD HERE',
+        'clientID': 'YOUR REDDIT APP CLIENT ID HERE',
+        'secretKey':'YOUR SECRED REDDIT API KEY HERE'}
